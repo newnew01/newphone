b7a14b80b238c1254ecde4680809b44743c079b4
newnew01 <newnew01@gmail.com>
2017-10-09 00:18:50 +0700

    sidebar template
    -devide sidebar template

---
+++ resources/views/template/sidebar/user-profile.blade.php
@@ -0,0 +1,34 @@
+<!-- User profile -->
+<div class="user-profile">
+    <!-- User profile image -->
+    <div class="profile-img"> <img src="../assets/images/users/profile.png" alt="user" />
+        <!-- this is blinking heartbit-->
+        <div class="notify setpos"> <span class="heartbit"></span> <span class="point"></span> </div>
+    </div>
+    <!-- User profile text-->
+    <div class="profile-text">
+        <h5>Markarn Doe</h5>
+        <a href="#" class="dropdown-toggle u-dropdown" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="true"><i class="mdi mdi-settings"></i></a>
+
+        <a href="pages-login.html" class="" data-toggle="tooltip" title="Logout"><i class="mdi mdi-power"></i></a>
+
+        <div class="dropdown-menu animated flipInY">
+            <!-- text-->
+            <a href="#" class="dropdown-item"><i class="ti-user"></i> My Profile</a>
+            <!-- text-->
+            <a href="#" class="dropdown-item"><i class="ti-wallet"></i> My Balance</a>
+            <!-- text-->
+            <a href="#" class="dropdown-item"><i class="ti-email"></i> Inbox</a>
+            <!-- text-->
+            <div class="dropdown-divider"></div>
+            <!-- text-->
+            <a href="#" class="dropdown-item"><i class="ti-settings"></i> Account Setting</a>
+            <!-- text-->
+            <div class="dropdown-divider"></div>
+            <!-- text-->
+            <a href="login.html" class="dropdown-item"><i class="fa fa-power-off"></i> Logout</a>
+            <!-- text-->
+        </div>
+    </div>
+</div>
+<!-- End User profile text-->
