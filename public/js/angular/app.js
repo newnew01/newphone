a7e8b04d3a1716a137fca6a959999c8057c168e9
newnew01 <newnew01@gmail.com>
2017-11-14 00:34:02 +0700

    update product search panel

--- public/js/angular/app.js
+++ public/js/angular/app.js
@@ -3,12 +3,8 @@ var app = angular.module('newphoneApp', ['ngSanitize'], function($interpolatePro
     $interpolateProvider.endSymbol('%>');
 });
 
-app.directive('focusOn', function() {
-    return function(scope, elem, attr) {
-        scope.$on(attr.focusOn, function(e) {
-            elem[0].focus();
-        });
-    };
+
+
+app.controller('TestController', function($scope,$http) {
+
 });
-
-
--- resources/views/pages/product-new.blade.php
+++ resources/views/pages/product-new.blade.php
@@ -10,7 +10,7 @@
 <!-- ============================================================== -->
 <!-- Start Page Content -->
 <!-- ============================================================== -->
-<div class="row" ng-app="newphoneApp">
+<div class="row">
     <div class="col-md-12">
         <div class="card">
             <div class="card-body p-b-0">
@@ -21,7 +21,6 @@
                         <div class="col-md-6">
                             <div class="card card-outline-inverse">
                                 <div class="card-header">
-                                    <button type="button" class="btn btn-sm waves-effect waves-light btn-rounded btn-success pull-right" data-toggle="modal" data-target="#checkProduct" data-whatever="@mdo" ><i class="mdi mdi-library-plus"></i> ตรวจสอบสินค้า</button>
                                     <h4 class="m-b-0 text-white">รายละเอียดสินค้า</h4>
                                 </div>
                                 <div class="card-body">
@@ -137,62 +136,6 @@
     </div>
     </div>
 
-    <div class="modal fade" id="checkProduct" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel1">
-        <div class="modal-dialog modal-lg" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h4 class="modal-title" id="exampleModalLabel1">ตรวจสอบสินค้า</h4>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                </div>
-                    <div class="modal-body"  ng-controller="ProductNewController">
-                        <div class="input-group">
-                            <div class="input-group-addon"><i class="mdi mdi-barcode"></i></div>
-                            <input type="text" class="form-control" name="barcode_check" id="input_barcode_check" placeholder="บาร์โค้ด" ng-model="barcode" ng-keyup="$event.keyCode == 13 && getProductInfo()">
-                            <span class="input-group-btn">
-                                <button ng-click="getProductInfo()"   class="btn waves-effect waves-light btn-success">ตรวจสอบ</button>
-                            </span>
-                        </div>
-                        <div class="row">
-                            <div class="col-md-7">
-                                <table class="table m-t-10">
-                                    <tbody>
-                                    <tr>
-                                        <td width="100px" bgcolor="#d3d3d3"><b>ชื่อสินค้า</b></td>
-                                        <td><% product.product_name %></td>
-                                    </tr>
-                                    <tr>
-                                        <td bgcolor="#d3d3d3">หมวดหมู่</td>
-                                        <td><% product.cate_name %></td>
-                                    </tr>
-                                    <tr>
-                                        <td bgcolor="#d3d3d3">ยี่ห้อ</td>
-                                        <td><% product.brand_name %></td>
-                                    </tr>
-                                    <tr>
-                                        <td bgcolor="#d3d3d3">รุ่น</td>
-                                        <td><% product.model %></td>
-                                    </tr>
-                                    <tr>
-                                        <td bgcolor="#d3d3d3">ราคา</td>
-                                        <td><% product.price %></td>
-                                    </tr>
-                                    <tr>
-                                        <td bgcolor="#d3d3d3">รายละเอียด</td>
-                                        <td><% product.description %></td>
-                                    </tr>
-                                    </tbody>
-                                </table>
-                            </div>
-                            <div class="col-md-5">
-                                <div class="p-t-20">
-                                    <img ng-src="<% product.image %>" src="/assets/images/no-image.png" width="100%">
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-            </div>
-        </div>
-    </div>
 </div>
 
 
@@ -204,6 +147,7 @@
 @section('js-head')
     <script type="text/javascript" src="/js/webcam/webcam.min.js"></script>
     <script src="/js/angular/controller/product-new.js"></script>
+
 @endsection
 
 @section('js-bottom')
