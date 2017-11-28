b81caf5049444d33ce6df63d573ea8188d5995eb
newnew01 <newnew01@gmail.com>
2017-11-29 01:03:27 +0700

    progress

---
+++ app/Http/Controllers/StockHistoryController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class StockHistoryController extends Controller
+{
+    public function createStockInHistory($products,$branch_id,$employee_id)
+    {
+
+    }
+
+    public function createStockTransferHistory()
+    {
+
+    }
+}
