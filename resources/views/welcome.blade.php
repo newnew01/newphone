1e82051044c734e8c2bd581505821c5a562c4c9a
newnew01 <newnew01@gmail.com>
2017-12-15 03:53:04 +0700

    111

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel XYZ
+                    Laravel
                 </div>
 
                 <div class="links">
