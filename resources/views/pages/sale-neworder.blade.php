0e21a1e5e4f3dc5ccbf25eab42a1b294a045a7bd
newnew01 <newnew01@gmail.com>
2017-12-17 04:05:24 +0700

    progress

--- resources/views/pages/sale-neworder.blade.php
+++ resources/views/pages/sale-neworder.blade.php
@@ -217,7 +217,7 @@
         <div class="modal-dialog modal-lg">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h4 class="modal-title" id="myLargeModalLabel">โอนจำนวนมาก</h4>
+                    <h4 class="modal-title" id="myLargeModalLabel">ขายจำนวนมาก</h4>
                     <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
                 </div>
                 <div class="modal-body">
